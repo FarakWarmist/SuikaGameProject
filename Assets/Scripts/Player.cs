b48f8e96fb57890819ee4ca49256f7efc715c263
William Sylvestre <farakwarts@gmail.com>
2024-09-15 18:41:45 -0400

    Update Scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,49 +21,50 @@ public class Player : MonoBehaviour
 
     [SerializeField] AudioSource dropAudio;
     [SerializeField] AudioSource fusionAudio;
-    
+
+    [SerializeField] Transform spawnOffset;
+
+    private GameObject currentBall;
+
     void Start()
     {
-        if(i == -1)
+        GrapBall();
+        /*if(i == -1)
         {
             i = Random.Range(0, metalObj.Length);
             j = Random.Range(0, metalObj.Length);
-        }
+        }*/
+
+    }
+    private void GrapBall()
+    {
+        int randomIndex = Random.Range(0, metalObj.Length);
+        var metalObjSpawn = metalObj[randomIndex];
+        currentBall = Instantiate(metalObjSpawn, spawnOffset.position, Quaternion.identity,spawnOffset);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        var metalObjSpawn = metalObj[i];
-        sprite = metalObjSpawn.GetComponent<SpriteRenderer>().sprite;
-        nextBall.GetComponent<SpriteRenderer>().sprite = sprite;
-        nextBall.transform.localScale = metalObjSpawn.GetComponent <Transform>().localScale;
-
-        secondNextBall.GetComponent<SpriteRenderer>().sprite = metalObj[j].GetComponent<SpriteRenderer>().sprite;
-        secondNextBall.transform.localScale = metalObj[j].GetComponent <Transform>().localScale;
-
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-        {
-            dropAudio.Play();
-            Instantiate(metalObjSpawn, transform.position, Quaternion.identity);
-
-            handOpened.SetActive(true);
-            handClosed.SetActive(false);
-            Invoke("CloseHand", 0.2f);
-
-            i = j;
-            j = Random.Range(0, metalObj.Length);
-        }
-
+    {        
         float x = Input.GetAxisRaw("Horizontal");
-        ///////////////////////////
         Vector3 nextPos = transform.position;// faire une copie
 
         nextPos += new Vector3(x, 0, 0) * Time.deltaTime * speed;
         nextPos.x = Mathf.Clamp(nextPos.x, 0.7f, 7.6f);
         transform.position = nextPos;
-        ///////////////////////////
 
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+
+            currentBall.transform.parent = null;
+            currentBall.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            dropAudio.Play();
+
+            handOpened.SetActive(true);
+            handClosed.SetActive(false);
+            Invoke("CloseHand", 0.2f);
+            Invoke("GrapBall", 0.5f);
+        }
     }
     void CloseHand()
     {
@@ -80,6 +81,7 @@ public class Player : MonoBehaviour
         }
         
         fusionAudio.Play();
-        Instantiate(metalObj[index], fusionPosition, Quaternion.identity);
+        var fusionedBall = Instantiate(metalObj[index], fusionPosition, Quaternion.identity);
+        fusionedBall.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
     }
 }
